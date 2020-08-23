5173225d28ff3c2109e76a03c940449bba692cab
Tarik Fajic <tfajic@gmail.com>
2020-08-23 15:50:31 +0200

    Updates

---
+++ js/backToTop.js
@@ -0,0 +1,14 @@
+//Header button
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass("show");
+  } else {
+    btn.removeClass("show");
+  }
+});
+
+btn.on("click", function () {
+  e.preventDefault();
+  $("html, body").animate({ scrollTop: 0 }, "300");
+});
